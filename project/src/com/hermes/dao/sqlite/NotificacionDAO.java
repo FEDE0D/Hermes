5bfc8a6daeffca08c5e90e0b974be70523f691da
Federico Pacheco <federicogpacheco@gmail.com>
2015-11-03 13:07:47 -0300

    eliminado codigo filtro anterior

--- project/src/com/hermes/dao/sqlite/NotificacionDAO.java
+++ project/src/com/hermes/dao/sqlite/NotificacionDAO.java
@@ -92,46 +92,6 @@ public class NotificacionDAO extends GenericDAO<Notificacion> implements INotifi
 		}
 	}
 	
-//	public List<Notificacion> filtrar(int idContenido, int idContexto, int idCategoria, int idPaciente, List idsEtiquetas){
-//		String contenido="idContenido", contexto= "idContexto",categoria="idCategoria",paciente="idPaciente";
-//		contenido+= (idContenido== 0) ? " > 0" : "= "+idContenido;
-//		contexto+= (idContexto== 0) ? " > 0" : "= "+idContexto;
-//		categoria+= (idCategoria== 0) ? " > 0" : "= "+idCategoria;
-//		paciente+= (idPaciente== 0) ? " > 0" : "= "+idPaciente;
-//		
-//		String consultaEtiquetas="";
-//		for (int i=0; i<idsEtiquetas.size(); i++) {
-//			if(i==0) consultaEtiquetas+="(";
-//			consultaEtiquetas+=" E.descripcion='"+idsEtiquetas.get(i)+"'";
-//			if (i!=idsEtiquetas.size()-1)consultaEtiquetas+=" OR ";
-//			else consultaEtiquetas+=") AND ";
-//			//System.out.println(idsEtiquetas.get(i));
-//		}
-////		System.out.println(consultaEtiquetas);
-//		String sql= "select  N.id,  N.idDevice, N.idContenido, N.idContexto, N.idPaciente, date,time, dateReceived, timeReceived, visto"
-//				+ " from Notificacion as N "
-//									 + "LEFT join Contenido as C on(N.idContenido = C.id)"
-//									 + "LEFT join NotificacionEtiqueta as NE on(NE.idNotificacion = N.id )"
-//									 + "LEFT join Etiqueta as E on(E.id = NE.idEtiqueta )"
-//				+ "where "+consultaEtiquetas + contenido + " AND "+ contexto + " AND "+ categoria + " AND "+ paciente;
-//		System.out.println(sql);
-//		try {
-//			Statement consulta = this.conexion.getEnlace().createStatement();
-//			ResultSet resultado= consulta.executeQuery(sql);
-//			List<Notificacion> listado  = new ArrayList<Notificacion>();
-//			while(resultado.next()){
-//				listado.add(this.createInstance(resultado));
-//			}
-//			resultado.close();
-//			return listado;
-//		}
-//		catch (SQLException e1) {
-//			e1.printStackTrace();
-//		}
-//		return null;
-//		
-//	}
-	
 	public List<Notificacion> filtrar(long idContenido, long idContexto, long idCategoria, long idPaciente, List<Long> idsEtiquetas, String dateFrom, String dateTo, String timeFrom, String timeTo) {
 		List<Notificacion> listado = new ArrayList<Notificacion>();
 		
