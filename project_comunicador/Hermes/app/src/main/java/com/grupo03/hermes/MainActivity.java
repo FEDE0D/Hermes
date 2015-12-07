aefe6f8ed0a7069de0b06fd4d23aee0100c8b411
Federico Pacheco <federicogpacheco@gmail.com>
2015-12-07 16:48:14 -0300

    update

--- project_comunicador/Hermes/app/src/main/java/com/grupo03/hermes/MainActivity.java
+++ project_comunicador/Hermes/app/src/main/java/com/grupo03/hermes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.grupo03.hermes;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,34 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        String[] datos = new String[]{
+                "Juan Perez",
+                "Francisco Hormiga",
+                "Pedrito Lomas",
+                "Juan Perez",
+                "Francisco Hormiga",
+                "Pedrito Lomas",
+                "Juan Perez",
+                "Francisco Hormiga",
+                "Pedrito Lomas",
+                "Juan Perez",
+                "Francisco Hormiga",
+                "Pedrito Lomas",
+                "Juan Perez",
+                "Francisco Hormiga",
+                "Pedrito Lomas"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, android.R.id.text1, datos);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), "Se eligió al alumno "+value, Toast.LENGTH_LONG).show();
             }
         });
     }
